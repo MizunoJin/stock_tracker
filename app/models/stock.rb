7b3a11a28f9c07926b3efd5779a06c086ef9d2ed
fantagreep <bump.lk_k.1215@icloud.com>
2021-08-23 08:05:21 +0900

    Display price

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,4 @@
 class Stock < ApplicationRecord
-
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'welcome#index'
+  get 'my_portofolio', to: 'users#my_portofolio'
+  get 'search_stock', to: 'stocks#search'
 end
