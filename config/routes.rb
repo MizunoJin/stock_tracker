b0867f21438d2cf10cc2643ae721d6d3ac6d8dd0
fantagreep <bump.lk_k.1215@icloud.com>
2021-08-26 00:00:07 +0900

    make friendsihp model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root to: 'welcome#index'
-  get 'my_portofolio', to: 'users#my_portofolio'
+  get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
 end
