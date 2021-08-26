8dfede9ce9c921ede3498d1e2c9f5cb009f3d677
fantagreep <bump.lk_k.1215@icloud.com>
2021-08-26 23:49:32 +0900

    Add delete function

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,5 +3,9 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
+    friendship = current_user.friendships.find_by(friend_id: params[:id])
+    friendship.destroy
+    flash[:notice] = "Friendship was successfully deleted from your portfolio"
+    redirect_to my_friends_path
   end
 end
